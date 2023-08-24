4e6a52ab7a633f7eddfb486b3babbf38bc9c7e8a
Neethusanal <neethukumarkk@gmail.com>
2023-08-24 16:34:04 +0530

    graph

---
+++ graphdfs.js
@@ -0,0 +1,31 @@
+const graph = {
+    A: ['B', 'D'],
+    B: ['A', 'C', 'E'],
+    C: ['B'],
+    D: ['A', 'E'],
+    E: ['B', 'D', 'F'],
+    F: ['E'],
+  };
+
+  function dfs(graph,start)
+  {
+    const stack=[start]
+    const visited=new Set()
+    const result=[]
+    while(stack.length)
+    {
+        const vertex=stack.pop()
+        if(!visited.has(vertex))
+        {
+            visited.add(vertex)
+            result.push(vertex)
+
+            for(const neighbor of graph[vertex])
+            {
+                stack.push(neighbor)
+            }
+        }
+    }
+    return result
+  }
+  console.log(dfs(graph,'A'))
