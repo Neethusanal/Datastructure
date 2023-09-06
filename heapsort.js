b9970d41535633e7f33cb023128b2e04825e5dda
Neethusanal <neethukumarkk@gmail.com>
2023-09-06 15:41:23 +0530

    "printmiddlethreeelements"

--- heapsort.js
+++ heapsort.js
@@ -5,29 +5,22 @@ function heapsort(arr)
     {
         heapify(arr,n,i)
     }
-    for(i=n-1;i>0;i--)
+    for(i=n-1;i>=0;i--)
     {
         let temp=arr[0]
         arr[0]=arr[i]
         arr[i]=temp
-
-        heapify(arr,i,0)
+         heapify(arr,i,0)
     }
     return arr
-
-
 }
-
-
-   
 
 function heapify(arr,n,i)
 {
     let largest=i
-    let left=2*i+1
+    let left= 2*i+1
     let right=2*i+2
-
-    if(left<n && arr[left]>arr[largest])
+    if(left<n&& arr[left]>arr[largest])
     {
         largest=left
     }
@@ -35,15 +28,13 @@ function heapify(arr,n,i)
     {
         largest=right
     }
-    if(largest!==i)
+    if(!largest==i)
     {
         let temp=arr[i]
-        arr[i]= arr[largest]
+        arr[i]=arr[largest]
         arr[largest]=temp
-        heapify(arr, n, largest);
     }
 }
-
-const arr=[8,2,-1,5,7,3,4]
-const res=heapsort(arr)
-console.log(res)
+let arr=[5,2,8,4,0,1]
+let sortedarr=heapsort(arr)
+console.log(sortedarr)
---
+++ middle.js
@@ -0,0 +1,14 @@
+function middle(){
+    let arr=[1,2,3,4,5,6,7,8,9,10]
+
+    let mid=(arr.length/2);
+    console.log(mid)
+    if(mid)
+    {
+        console.log(arr[mid-1],arr[mid],arr[mid+1])
+    }
+  
+
+}
+
+middle()
